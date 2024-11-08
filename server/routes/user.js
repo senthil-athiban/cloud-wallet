7389e707b997fdc33da47d6567f3be7967b58b5e
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-08 23:13:08 +0530

    feat: add routes

---
+++ server/routes/user.js
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import User from "../models/user.js";
+import { Keypair } from "@solana/web3.js";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const router = Router();
+
+router.post("/signup", async (req, res) => {
+    const { username, password} = req.body;
+    
+    const newKeyPair = Keypair.generate();
+
+    const user = await User.findOne({
+        username
+    });
+
+    if(user){
+        return res.json({status: 409, message: "User already exists"});
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    await User.create({
+        username,
+        password: hashedPassword,
+        publicKey: newKeyPair.publicKey.toString(),
+        privateKey: newKeyPair.secretKey.toString()
+    });
+
+    return res.json({publicKey: newKeyPair.publicKey.toString()});
+});
+
+router.post("/signin", async (req, res) => {
+    const { username, password} = req.query;
+    const user = await User.findOne({username});
+
+    if(!user){
+        return res.json({status: 404, message: "User not exists"})
+    }
+
+    const checkPassword = bcryptjs.compare(password, user.password);
+
+    if(!checkPassword){
+        return res.json({status: 401, message: "Invalid credentials"})
+    }
+
+    const token = jwt.sign({
+        id: user.id
+    }, JWT_SECRET);
+})
+
+export const userRouter = router;
